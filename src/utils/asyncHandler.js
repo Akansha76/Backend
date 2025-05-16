2d77ea3156e947c37cd0dc6853e5729c9171073a
Akansha76 <shakyaakanksha67@gmail.com>
2025-05-16 21:50:27 +0530

    util code

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,25 @@
+const asyncHandler=(requetHandler)=>{
+    (req,res,next)=>{
+        Promise.resolve(requestHandler(req,res,next)).catch((err)=>next(err))
+    }
+}
+
+
+export {asyncHandler}
+// const asyncHandler=()=>{}
+// const asyncHandler=(func)=>()=>{
+    
+// }
+// const asyncHandler=(fn)=>async(req,res,next)=>{
+//     try{
+//         await fn(req,res)
+
+//     }
+//     catch(error){
+//         res.status(error.code||500).json({
+//             success:false,
+//             message:error.message
+//         })
+//     }
+
+// }
