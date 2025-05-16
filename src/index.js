371145a125c1cdd4ca809a02e2a556e9a1cce1ce
Akansha76 <shakyaakanksha67@gmail.com>
2025-05-16 18:15:31 +0530

    Fix MongoDB connection and update scripts

--- src/index.js
+++ src/index.js
@@ -1,5 +1,33 @@
-import mongoose from 'mongoose';
-import {DB_NAME} from "./constants";
+//require('dotenv').config({path:'./env'})
+ import dotenv from "dotenv"
+// import connectDB from "./db/index.js"
+//import mongoose from 'mongoose';
+//import {DB_NAME} from "./constants";
+import connectDB from './db/index.js';
+
+
+dotenv.config({
+    path:'./env'
+})
+
+connectDB()
+// .then(()=>{
+//     application.listen(process.env.PORT||8000,()=>
+//         console.log(`Server is running at port: ${process.env.PORT}`)
+//     )
+// })
+// .catch((err)=>{
+//     console.log("MONGO db connection falied",err)
+// })
+
+
+
+
+
+
+
+
+
 
 
 
